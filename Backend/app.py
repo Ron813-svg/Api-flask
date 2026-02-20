ed5e68608651e8f219f3658b2f9a11411ac0b172
Rony <ronyjavierramirezalvarado@gmail.com>
2026-02-19 21:01:27 -0600

    V6

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.temperature_routes import temperature_bp
+import os
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5500"}})
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 
 app.register_blueprint(temperature_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)        
